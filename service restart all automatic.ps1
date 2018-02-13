a4255f348c789acbb514a3611a31981f1338320a
invertigo260 <vert260@gmail.com>
2018-02-13 10:18:09 -0700

    started processes set to automatic, but not in running status

---
+++ service restart all automatic.ps1
@@ -0,0 +1,47 @@
+﻿param (
+
+[Parameter(Mandatory=$True,ValueFromPipeline=$true)]
+$server
+
+)
+
+#gets inital list of services
+$servicelist = Get-Service -ComputerName $server
+
+
+Write-Host "All services on $($server) that are Automatic but not running:"
+
+foreach ($service in $servicelist) {
+if ($service.StartType -eq "Automatic" -and $service.Status -ne "Running" ) {
+
+#$betterlist += $service
+Write-host $service.name $service.starttype $service.status
+
+
+
+} #if
+} #foreach
+
+
+Write-Host "Starting listed services"
+
+foreach ($service in $servicelist) {
+if ($service.StartType -eq "Automatic" -and $service.Status -ne "Running" ) {
+
+$service | Start-Service
+
+
+
+} #if
+} #foreach
+
+
+write-Host "All services on $($server) that are Automatic but not running after start, and their new status:"
+
+foreach ($service in $servicelist) {
+if ($service.StartType -eq "Automatic" -and $service.Status -ne "Running" ) {
+
+#$betterlist += $service
+Write-host $service.name $service.starttype $service.status
+} #if
+} #foreach
